ab5e4670c08a9a0b92934e323d91774d68ed9ab6
abdo <eemgyg@gmail.com>
2023-10-18 01:07:49 +0200

    fix

--- _printf.c
+++ _printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	if (strcmp(format, "") <= 1)
+	if (strcmp(format, "") == 0 )
 		return (-1);
 	va_start(args, format);
 	while (*format && format)
