dbf6fd7cb91e5e1b218a59601dfe0aefc19b4a32
Abdoemg <eemgyg@gmail.com>
2023-10-17 18:23:30 +0200

    empty string check

--- _printf.c
+++ _printf.c
@@ -20,7 +20,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	while (*format != '\0')
+	while (*format != '\0' && *format)
 	{
 		if (*format == '%')
 		{
