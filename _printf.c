371b09c8d513e79ec817d1aa8985f066341987fc
Abd03mg <89263478+Abd03mg@users.noreply.github.com>
2023-10-17 19:39:58 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -19,7 +19,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	if (strcmp(*format, "") == 0)
+	if (strcmp(format, "") <= 1)
 		return (-1);
 	while (*format && format)
 	{
