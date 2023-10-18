d23eabf207b0ae3b07e5798a911b767643e2f420
Abd03mg <89263478+Abd03mg@users.noreply.github.com>
2023-10-18 03:08:50 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -13,8 +13,7 @@ int _printf(const char *format, ...)
 	int len = 0;
 	va_list args;
 
-	//if (!format || (format[0] == '%' && format[1] == '\0'))
-		//return (-1);
+
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	va_start(args, format);
